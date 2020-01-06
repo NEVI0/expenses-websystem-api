c9c3e68ee0d50a1af87a628a13ce1c268e7b7e90
Nevio <neviocostamagagnin@gmail.com>
2020-01-06 20:28:31 -0200

    Finishing the Expenses CRUD

--- src/controllers/expenseCtrl.js
+++ src/controllers/expenseCtrl.js
@@ -7,47 +7,60 @@ const Expenses = mongoose.model("Expense");
 
 /* ===================== Controllers ===================== */
 
+/* Busca todas as Despesas */
+const getExpenses = (req, res, next) => {
+    Expenses.find((err, resp) => {
+        if (err) {
+            return res.status(503).json(err); /* 1 - Se houver algum error, o retorna */
+        } else {
+            return res.status(200).json(resp); /* 2 - Senão retorna as Despesas */
+        }
+    });
+}
+
 /* Busca todas das Despesas pelo ID do Usuário */
 const getExpensesByUserId = (req, res, next) => {
     Expenses.find({ userId: req.params.userId }, (err, resp) => {
         if (err) {
-            return res.json(err);
+            return res.status(503).json(err); /* 1 - Se houver algum error, o retorna */
         } else {
-            return res.status(200).json(resp);
+            return res.status(200).json(resp); /* 2 - Senão retorna as Despesas */
         }
     });
 }
 
 /* Inseri uma nova Despesa */
 const insertExpense = (req, res, next) => {
-    Expenses.create({
-        name: req.body.name,
-        value: req.body.value,
-        createdAt: req.body.createdAt,
-        description: req.body.description,
-        userId: req.body.userId
-    }, (err, resp) => {
+    Expenses.create(req.body, (err, resp) => {
         if (err) {
-            return res.json(err);
+            return res.status(503).json(err); /* 1 - Se houver algum error, o retorna */
         } else {
-            return res.json({ message: "Despesa inserida com Sucesso!" });
+            return res.status(200).json(resp); /* 2 - Senão retorna a despesa cadastrada */
         }
     });
 }
 
 /* Atualiza uma Expencia pelo ID */
-const updateExpenseById = (req, res, next) => {}
+const updateExpense = (req, res, next) => {
+    Expenses.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, resp) => {
+        if (err) {
+            return res.status(503).json(err); /* 1 - Se houver algum error, o retorna */
+        } else {
+            return res.status(200).json(resp); /* 2 - Senão retorna a despesa atualizada */
+        }
+    });
+}
 
 /* Deleta uma Expense pelo ID */
-const deleteExpenseById = (req, res, next) => {
+const deleteExpense = (req, res, next) => {
     Expenses.findByIdAndDelete(req.params.id, (err, resp) => {
         if (err) {
-            return res.json(err);
+            return res.status(503).json(err); /* 1 - Se houver algum error, o retorna */
         } else {
-            return res.status(200).send("Despesa deletada com Sucesso");
+            return res.status(200).json({ msg: "Despesa deletada com sucesso" }); /* 2 - Senão uma mensagem de sucess */
         }
     });
 }
 
 /* Exporta os Controllers para as rotas */
-module.exports = { getExpensesByUserId, insertExpense, updateExpenseById, deleteExpenseById }
+module.exports = { getExpenses, getExpensesByUserId, insertExpense, updateExpense, deleteExpense }
