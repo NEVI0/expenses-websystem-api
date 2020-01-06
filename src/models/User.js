03492fda00258106ac60a0a9ab80f2e5f89bc20a
Nevio <neviocostamagagnin@gmail.com>
2020-01-06 16:04:24 -0200

    Fixing the user model

--- src/models/User.js
+++ src/models/User.js
@@ -5,7 +5,10 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    salary: { type: Number, required: true },
+    imgName: { type: String },
+    imgUrl: { type: String }
 });
 
 /* Exporta o Schema / Model */
