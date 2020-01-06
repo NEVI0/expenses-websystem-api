7ef57005d5fcef0778bb02e0b8923038441919a5
Nevio <neviocostamagagnin@gmail.com>
2020-01-06 16:04:09 -0200

    Adding new routes to User CRUD

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,6 +3,7 @@ const express = require("express");
 
 /* Controllers */
 const ExpensesCtrl = require("../controllers/expenseCtrl");
+const UserCtrl = require("../controllers/userCtrl");
 
 /* Exporta as Rotas para o arquivo Inicializador */
 module.exports = (server) => {
@@ -11,12 +12,29 @@ module.exports = (server) => {
     const openApi = express.Router();
     server.use("/oapi", openApi);
 
-    /* Rotas Abertas */
+    /* Rotas Abertas - Rota Inicial */
     openApi.get("/", (req, res, next) => {
         return res.status(200).send(`API's Running - Status: OK`);   
     });
 
-    /* Rotas Abertas Temporariamente */
+    /* =========== Rotas de Usuario =========== */
+
+    /* Rotas Abertas */
+    openApi.post("/signup", UserCtrl.signup);
+    openApi.post("/login", UserCtrl.login);
+
+    /* Rotas Fechadas */
+    openApi.get("/users", UserCtrl.getUsers);
+    openApi.get("/user/:id", UserCtrl.getUserById);
+
+    openApi.put("/userSimple/:id", UserCtrl.updateUserSimple);
+    openApi.put("/userAdvanced/:id", UserCtrl.updateUserAdvanced);
+
+    openApi.delete("/user/:id", UserCtrl.deleteUser);
+    
+    /* =========== Rotas de Despesas =========== */
+
+    /* Rotas Fechadas */
     openApi.get("/expenses/:userId", ExpensesCtrl.getExpensesByUserId);
     openApi.post("/expenses", ExpensesCtrl.insertExpense);
     openApi.put("/expenses/:id", ExpensesCtrl.updateExpenseById);
