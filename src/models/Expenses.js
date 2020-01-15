81894bc76a334da745568c88be03ed0174c39174
Nevio <neviocostamagagnin@gmail.com>
2020-01-15 11:00:42 -0200

    Changing the Expenses Model

--- src/models/Expenses.js
+++ src/models/Expenses.js
@@ -3,12 +3,39 @@ const mongoose = require("mongoose");
 const mongoosePaginate = require("mongoose-paginate");
 
 /* Schema / Model de Expenses */
+// const expensesSchema = new mongoose.Schema({
+//     name: { type: String, required: true },
+//     value: { type: Number, required: true },
+//     description: { type: String, required: true },
+//     createdAt: { type: String, required: true },
+//     userId: { type: String, required: true }
+// });
+
 const expensesSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    value: { type: Number, required: true },
-    description: { type: String, required: true },
-    createdAt: { type: String, required: true },
-    userId: { type: String, required: true }
+    name: { 
+        type: String, required: true
+    },
+    value: { 
+        type: Number, required: true
+    },
+    description: { 
+        type: String, required: true
+    },
+    date: { 
+        type: Date, required: true
+    },
+    createdAt: { 
+        type: Date, default: Date.now 
+    },
+    userId: { 
+        type: String, required: true 
+    },
+    userData: { 
+        type: Array, required: true
+    },
+    tags: {
+        type: Array, required: true
+    }
 });
 
 /* Adiciona a paginação */
