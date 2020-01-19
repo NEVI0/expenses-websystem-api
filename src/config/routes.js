1e75e5b72155b513182f3e690ff3aa5e6ebc67d3
Nevio <neviocostamagagnin@gmail.com>
2020-01-19 13:53:26 -0200

    Creating a new route to search expense

--- src/config/routes.js
+++ src/config/routes.js
@@ -48,42 +48,8 @@ module.exports = (server) => {
     api.get("/expenses/:userId", ExpensesCtrl.getExpensesByUserId);
     api.get("/dataController/:userId", ExpensesCtrl.getDataController);
     api.post("/expenses", ExpensesCtrl.insertExpense);
+    api.post("/search", ExpensesCtrl.search);
     api.put("/expenses/:id", ExpensesCtrl.updateExpense);
     api.delete("/expenses/:id", ExpensesCtrl.deleteExpense);
 
 }
-
-/* Configura as rotas Abertas */
-// const openApi = express.Router();
-// server.use("/oapi", openApi);
-
-// /* Rotas Abertas - Rota Inicial */
-// openApi.get("/", (req, res, next) => {
-//     return res.status(200).send(`API's Running - Status: OK`);   
-// });
-
-// /* =========== Rotas de Usuario =========== */
-
-// /* Rotas Abertas */
-// openApi.post("/signup", UserCtrl.signup);
-// openApi.post("/login", UserCtrl.login);
-
-// /* Rotas Fechadas */
-// openApi.get("/users", UserCtrl.getUsers);
-// openApi.get("/user/:id", UserCtrl.getUserById);
-
-// openApi.put("/userSimple/:id", UserCtrl.updateUserSimple);
-// openApi.put("/userAdvanced/:id", UserCtrl.updateUserAdvanced);
-
-// openApi.delete("/user/:id", UserCtrl.deleteUser);
-    
-// /* =========== Rotas de Despesas =========== */
-
-// /* Rotas Fechadas */
-// openApi.get("/allExpenses", ExpensesCtrl.getExpenses);
-// openApi.get("/lastExpenses/:userId", ExpensesCtrl.getLastTen);
-// openApi.get("/expenses/:userId", ExpensesCtrl.getExpensesByUserId);
-// openApi.post("/expenses", ExpensesCtrl.insertExpense);
-// openApi.put("/expenses/:id", ExpensesCtrl.updateExpense);
-// openApi.delete("/expenses/:id", ExpensesCtrl.deleteExpense);
-// openApi.get("/userController/:userId", ExpensesCtrl.getDataController);
