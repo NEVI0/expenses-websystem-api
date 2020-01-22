da9a75e5785d1a9f8d4edc5ac503ab487176855d
Nevio <neviocostamagagnin@gmail.com>
2020-01-22 11:08:39 -0200

    Adding options

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,6 +1,6 @@
-module.exports = function(req, res, next) {
+module.exports = (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 }
