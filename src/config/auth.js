7590a2d6dbee3c6e99a8fe6e3dc6a45c1769a4b6
Nevio <neviocostamagagnin@gmail.com>
2020-01-23 20:57:24 -0200

    Adding the Async / Await and Try / Catch to the code

--- src/config/auth.js
+++ src/config/auth.js
@@ -5,11 +5,11 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 /* Exporta a função middleware para bloaquear as rotas */
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
 
     /* Verifica se o preflight é OPTIONS */
     if (req.method == "OPTIONS") {
-        next(); /* Chama o próximo middleware */
+        next();
     } else {
 
         /* Pega o Token */
@@ -20,17 +20,21 @@ module.exports = (req, res, next) => {
             return res.status(403).json({ errorMsg: "No token Provided" });
         }
 
-        /* Verifica o Token */
-        jwt.verify(token, process.env.AUTH_SECRET, (err, decoded) => {
-            /* 1 - Retorna o error se houver algum */
-            /* 2 - Coloca o token a variavel decoed e chama o proximo middleware */
-            if (err) {
-                return res.status(403).json({ errorMsg: "Failed to authenticate token" });
-            } else {
-                req.decoded = decoded;
-                next();
-            }
-        });
+		try {
+			/* Verifica o Token */
+			await jwt.verify(token, process.env.AUTH_SECRET, (err, decoded) => {
+				/* 1 - Retorna o error se houver algum */
+				/* 2 - Coloca o token a variavel decoed e chama o proximo middleware */
+				if (err) {
+					return res.status(403).json({ errorMsg: "Failed to authenticate token" });
+				} else {
+					req.decoded = decoded;
+					next();
+				}
+			});
+		} catch (err) {
+			return res.status(400).json(err);
+		}
 
     }
 
--- src/controllers/userCtrl.js
+++ src/controllers/userCtrl.js
@@ -6,8 +6,9 @@
 
 /* Dependencias */
 const mongoose = require("mongoose");
-const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const nodeMailer = require("nodemailer");
 
 /* Traz os Schemas / Models */
 require("../models/User");
@@ -22,33 +23,43 @@ const passwordRegex = /((?=.*[a-z])(?=.*[A-Z]).{7})/;
 /* Habilita as variaveis de ambiente */
 require("dotenv").config();
 
+/* Traz as configurações do email */
+const mailConfig = require("../config/mailConfig");
 
 /* ===================== Controllers ===================== */
 
 /* Busca todos os usuários */
-const getUsers = (req, res, next) => {
-    User.find((err, resp) => {        
-        if (err) {
-            return res.status(404).json(err); /* 1 - Se houver algum error, o retorna */
-        } else {
-            return res.status(200).json(resp); /* 2 - Senão retorna o usuário */
-        }
-    });
+const getUsers = async (req, res) => {
+    try {
+        await User.find((err, resp) => {        
+            if (err) {
+                return res.status(404).json(err); /* 1 - Se houver algum error, o retorna */
+            } else {
+                return res.status(200).json(resp); /* 2 - Senão retorna o usuário */
+            }
+        });
+    } catch (err) {
+        return res.status(400).json(err);
+    }
 }
 
 /* Busca dados do usuário pelo ID */
-const getUserById = (req, res, next) => {
-    User.findById(req.params.id, (err, resp) => {
-        if (err) {
-            return res.status(404).json(err); /* 1 - Se houver algum error, o retorna */
-        } else {
-            return res.status(200).json(resp); /* 2 - Senão retorna o usuário */
-        }
-    });
+const getUserById = async (req, res) => {
+    try {
+        await User.findById(req.params.id, (err, resp) => {
+            if (err) {
+                return res.status(404).json(err); /* 1 - Se houver algum error, o retorna */
+            } else {
+                return res.status(200).json(resp); /* 2 - Senão retorna o usuário */
+            }
+        });
+    } catch (err) {
+        return res.status(400).json(err);
+    }
 }
 
 /* Cria um usuário / Signup */
-const signup = (req, res, next) => {
+const signup = async (req, res) => {
 
     /* Pega as informações do usuário */
     const name = req.body.name || "";
@@ -73,117 +84,122 @@ const signup = (req, res, next) => {
         return res.status(404).json({ errorMsg: "As Senhas não são iguais." });
     }
 
-    /* Criptografa a senha */
-    const cipher = crypto.createCipher(process.env.CRYPTO_ALG, process.env.CRYPTO_SECRET);
-    cipher.update(password);
-    const passwordHash = cipher.final("hex");
+    /* Faz a cryptografia da senha e coloca o hash na senha */
+    const salt = bcrypt.genSaltSync();
+    const passwordHash = bcrypt.hashSync(password, salt);
 
-    /* Busca no banco um usuário existente */
-    User.findOne({ email: email }, (err, user) => {
+    try {
+        /* Busca no banco um usuário existente */
+        await User.findOne({ email: email }, (err, user) => {
 
-        /* Se houver algum error, o retorna */
-        if (err) {
-            return res.status(404).json(err);
-        }
+            /* Se houver algum error, o retorna */
+            if (err) {
+                return res.status(404).json(err);
+            }
 
-        /* 1 - Se existir um usuário, retorna um error */
-        /* 2 - Cria um novo usuário */
-        if (user) {
-            return res.status(404).json({ errorMsg: "O Usuário já existe." });
-        } else {
+            /* 1 - Se existir um usuário, retorna um error */
+            /* 2 - Cria um novo usuário */
+            if (user) {
+                return res.status(404).json({ errorMsg: "O Usuário já existe." });
+            } else {
 
-            /* Cria um usuário */
-            User.create({
-                name: name,
-                email: email,
-                password: passwordHash,
-                salary: 1000,
-                imgName: "376574365784-imagem.png",
-                imgUrl: "https://amazon-s3.com/debbuing"
-            }, (err, resp) => {
+                /* Cria um usuário */
+                User.create({
+                    name: name,
+                    email: email,
+                    password: passwordHash,
+                    salary: 1000,
+                    imgName: "376574365784-imagem.png",
+                    imgUrl: "https://amazon-s3.com/debbuing"
+                }, (err, resp) => {
 
-                /* Se houver algum error, o retorna */
-                if (err) {
-                    return res.status(404).json(err);
-                }
+                    /* Se houver algum error, o retorna */
+                    if (err) {
+                        return res.status(404).json(err);
+                    }
 
-                /* Cria o Token */
-                const token = jwt.sign(resp.toJSON(), process.env.AUTH_SECRET, { expiresIn: "1 day" });
+                    /* Cria o Token e envia as informações do usuário para o client */
+                    const token = jwt.sign(resp.toJSON(), process.env.AUTH_SECRET, { expiresIn: "1 day" });
+                    const { _id, name, email, salary, imgName, imgUrl } = resp;
+                    return res.status(200).json({ _id, name, email, salary, imgName, imgUrl, token });
 
-                /* Extrai as informações do usuário */
-                const { _id, name, email, salary, imgName, imgUrl } = resp;
+                });
 
-                /* Retorna para o cliente (Frontend) os dados */
-                return res.status(200).json({ _id, name, email, salary, imgName, imgUrl, token });
+            }
 
-            });
-
-        }
-
-    });
+        });
+    } catch (err) {
+        return res.status(400).json(err);
+    }
 
 }
 
 /* Autenticação de usuário / Login */
-const login = (req, res, next) => {
+const login = async (req, res) => {
 
     /* Pega as informações do usuário */
     const email = req.body.email || "";
     const password = req.body.password || "";
-
-    /* Criptografa a senha */
-    const cipher = crypto.createCipher(process.env.CRYPTO_ALG, process.env.CRYPTO_SECRET);
-    cipher.update(password);
-    const passwordHash = cipher.final("hex");
     
-    /* Procura o usuário */
-    User.findOne({ email: email }, (err, user) => {
-        
-        /* Verifica se existe algum error */
-        if (err) {
-            return res.status(404).json(err);
-        }
-
-        /* Verifica se o usuário existe */
-        if (!user) {
-            return res.status(404).json({ errorMsg: "O usuário não existe" });
-        }   
-        
-        /* Verifica se as senha são iguais */
-        if (passwordHash === user.password) {
+    try {
+        /* Procura o usuário */
+        await User.findOne({ email: email }, (err, user) => {
             
-            /* Cria o Token do usuário */
+            /* Verifica se existe algum error */
+            if (err) {
+                return res.status(404).json(err);
+            }
+
+            /* Verifica se o usuário existe */
+            if (!user) {
+                return res.status(404).json({ errorMsg: "O usuário não existe" });
+            }   
+            
+            /* Verifica se as senha são iguais */
+            if (bcrypt.compareSync(password, user.password)) {
+                
+                /* Cria o Token do usuário e envia as informações para o cliente */
+                const token = jwt.sign(user.toJSON(), process.env.AUTH_SECRET, { expiresIn: "1 day" });
+                const { _id, name, email, salary, imgUrl } = user;
+                return res.status(200).json({ _id, name, email, salary, imgUrl, token });
+
+            } else {
+                return res.status(404).json({ errorMsg: "Email ou Senha inválidos" });
+            }
+
+        });
+    } catch (err) {
+        return res.status(400).json(err);
+    }   
+
+}
+
+/* Atualização de usuário */
+const updateUserSimple = async (req, res) => {
+    try {
+        /* Atualiza os dados o usuário */
+        await User.findByIdAndUpdate(req.params.id, req.body, { 
+            new: true 
+        }, (err , user) => {
+
+            /* Se houver algum error, o retorna */
+            if (err) {
+                return res.status(404).json(err);
+            }
+            
+            /* Cria o Token do usuário e envia as informções para o client */
             const token = jwt.sign(user.toJSON(), process.env.AUTH_SECRET, { expiresIn: "1 day" });
             const { _id, name, email, salary, imgUrl } = user;
             return res.status(200).json({ _id, name, email, salary, imgUrl, token });
 
-        } else {
-            return res.status(404).json({ errorMsg: "Email ou Senha inválidos" });
-        }
-
-    });
-
+        });
+    } catch (err) {
+        return res.status(400).json(err);
+    }
 }
 
 /* Atualização de usuário */
-const updateUserSimple = (req, res, next) => {
-	User.findByIdAndUpdate(req.params.id, req.body, { 
-		new: true 
-	}, (err , user) => {
-		if (err) {
-			return res.status(404).json(err); /* Se houver algum error, o retorna */
-		}
-		
-		/* Cria o Token do usuário */
-		const token = jwt.sign(user.toJSON(), process.env.AUTH_SECRET, { expiresIn: "1 day" });
-		const { _id, name, email, salary, imgUrl } = user;
-		return res.status(200).json({ _id, name, email, salary, imgUrl, token });
-
-	});
-}
-
-/* Atualização de usuário */
-const updateUserAdvanced = (req, res, next) => {
+const updateUserAdvanced = async (req, res) => {
     
     /* Pega as informações do usuário */
     const oldPassword = req.body.old_password || "";
@@ -207,54 +223,116 @@ const updateUserAdvanced = (req, res, next) => {
         return res.status(400).json({ errorMsg: "As Senhas não são iguais." });
     }
 
-    /* Criptografa a senha */
-    const cipher = crypto.createCipher(process.env.CRYPTO_ALG, process.env.CRYPTO_SECRET);
-    cipher.update(password);
-    const passwordHash = cipher.final("hex");
+    /* Faz a cryptografia da senha e coloca o hash na senha */
+    const salt = bcrypt.genSaltSync();
+    const passwordHash = bcrypt.hashSync(password, salt);
 
-    /* Atualiza a Senha */
-    User.findByIdAndUpdate(req.params.id, {
-        password: passwordHash
-    }, { new: true }, (err, resp) => {
-        if (err) {
-            return res.status(404).json(err); /* 1 - Se houver algum error, o retorna */
-        } else {
-            return res.status(200).json(resp); /* 2 - Senão retorna os dados do usuário */
-        }
-    });
+    try {
+        /* Atualiza a Senha */
+        await User.findByIdAndUpdate(req.params.id, {
+            password: passwordHash
+        }, { new: true }, (err, resp) => {
+            if (err) {
+                return res.status(404).json(err); /* 1 - Se houver algum error, o retorna */
+            } else {
+                return res.status(200).json(resp); /* 2 - Senão retorna os dados do usuário */
+            }
+        });
+    } catch (err) {
+        return res.status(400).json(err);
+    }
 
 }
 
 /* Exclução de Usuário */
-const deleteUser = (req, res, next) => {
-    User.findByIdAndDelete(req.params.id, (err, resp) => {
-        if (err) {
-            return res.status(404).json(err); /* Se houver algum error, o retorna */
-		} 
-		
-		Expenses.deleteMany({ userId: req.params.id }, (err) => {
-			if (err) {
-				return res.status(404).json(err); /* Se houver algum error, o retorna */
-			} else {
-				return res.status(200).json({ msg: "Usuário deletado com sucesso." });
-			}
-		});		
-    });
+const deleteUser = async (req, res) => {
+    try {
+        /* Deleta o usário */
+        await User.findByIdAndDelete(req.params.id, (err, resp) => {
+            /* Se houver algum error, o retorna */
+            if (err) {
+                return res.status(404).json(err); 
+            } 
+            
+            /* Deleta todas as despesas do usuário */
+            Expenses.deleteMany({ userId: req.params.id }, (err) => {
+                if (err) {
+                    return res.status(404).json(err); /* Se houver algum error, o retorna */
+                } else {
+                    return res.status(200).json({ msg: "Usuário deletado com sucesso." });
+                }
+            });		
+        });
+    } catch (err) {
+        return res.status(400).json(err);
+    }
 }
 
 /* Valida o Token do usuário */
-const validateToken = (req, res, next) => {
+const validateToken = async (req, res) => {
     /* Pega a informação do Token */
     const token = req.body.token || ""; 
 
-    /* Verifica o token */
-    jwt.verify(token, process.env.AUTH_SECRET, function(err, decoded) {
-        if (err) {
-            return res.status(404).json({ valid: false });
-        } else {
-            return res.status(200).json({ valid: true });
-        }
-    });
+    try {
+        /* Verifica o token */
+        await jwt.verify(token, process.env.AUTH_SECRET, function(err, decoded) {
+            if (err) {
+                return res.status(404).json({ valid: false });
+            } else {
+                return res.status(200).json({ valid: true });
+            }
+        });
+    } catch (err) {
+        return res.status(400).json(err);
+    }
+}
+
+/* Recuperação de senha */
+const recoveryPassword = (req, res) => {
+    try {
+        return res.status(200).json({ msg: "Dummy Text!" });
+    } catch (err) {
+        return res.status(400).json(err);
+    }
+	/* Pega o email */
+	// const email = req.body.email || "";
+
+	// /* Procura no banco o usuário pelo email */
+	// User.findOne({ email: email }, (err, user) => {
+
+	// 	/* Se tiver errors, retorna */
+	// 	if (err) {
+	// 		return res.status(404).json(err);
+	// 	}
+
+	// 	/* Verifica se o usuário não existe */
+	// 	if (!user) {
+	// 		return res.status(404).json({ errorMsg: "O usuário com este email não existe" });
+	// 	}
+
+	// 	/* Configura o email */
+	// 	const transporter = nodeMailer.createTransport(mailConfig);
+
+	// 	/* Envia o email */
+	// 	transporter.sendMail({
+	// 		from: `Minhas Despesas - Sistema Gerenciador de Despesas Pessoais <${process.env.MAIL_EMAIL}>`,
+	// 		to: email,
+	// 		subject: "Recuperação de Senha",
+	// 		text: `Sua senha é: ${user.password}`,
+	// 		html: `Sua senha é: ${user.password}`
+	// 	}, (err, info) => {
+	// 		if (err) {
+	// 			console.log(`Error: \n ${err}`);
+	// 		} else {
+	// 			console.log(`Information: \n ${info}`);
+	// 		}
+	// 	});
+		
+	// 	/* Envia uma mensagem de sucesso */
+	// 	return res.status(200).json({ msg: `Um email foi enviado para: ${email}` });
+
+	// });
+
 }
 
 /* Exporta os Controllers para as rotas */
@@ -266,7 +344,8 @@ module.exports = {
     updateUserSimple,
     updateUserAdvanced, 
     deleteUser,
-    validateToken
+	validateToken,
+	recoveryPassword
 }
 
 /* ========================= Importante ========================= */
