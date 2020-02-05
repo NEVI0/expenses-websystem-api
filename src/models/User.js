708b471fc25e0169fcf282399e12100a60869dc7
Nevio <neviocostamagagnin@gmail.com>
2020-02-05 15:40:45 -0200

    Adding the imgUrl

--- src/models/User.js
+++ src/models/User.js
@@ -7,7 +7,8 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
     salary: { type: Number, required: true },
-    imgName: { type: String }
+	imgName: { type: String },
+	imgUrl: { type: String }
 });
 
 /* Export the Schema / Model */
