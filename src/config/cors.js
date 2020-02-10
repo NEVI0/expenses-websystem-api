9d3e88f227242ff448b9338b36f84ce3054984f5
Nevio <neviocostamagagnin@gmail.com>
2020-02-10 21:44:20 -0200

    Teste

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "https://expenses-web-system.firebaseapp.com");
 	res.header("Access-Control-Allow-Origin", "https://expenses-web-system-reset-pass.firebaseapp.com");
-	res.header("Access-Control-Allow-Credentials", "true")
+	res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
