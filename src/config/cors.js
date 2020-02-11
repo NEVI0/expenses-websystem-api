accc631d963af4e42545b6d8a0b8c7dc8991b5c5
Nevio <neviocostamagagnin@gmail.com>
2020-02-11 14:47:17 -0200

    Jusf for Debbug

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,11 +1,11 @@
 module.exports = (req, res, next) => {
-	const allowedOrigins = ["https://expenses-web-system.firebaseapp.com", "https://expenses-web-system-reset-pass.firebaseapp.com"];
-	const origin = req.headers.origin;
+	// const allowedOrigins = ["https://expenses-web-system.firebaseapp.com", "https://expenses-web-system-reset-pass.firebaseapp.com"];
+	// const origin = req.headers.origin;
 
-	if (allowedOrigins.indexOf(origin) > -1) {
-		res.setHeader("Access-Control-Allow-Origin", origin);
-	}
-
+	// if (allowedOrigins.indexOf(origin) > -1) {
+	// 	res.setHeader("Access-Control-Allow-Origin", origin);
+	// }
+	res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
